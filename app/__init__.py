3ea777452fd680f67fbb239f0d57b6c1eb5669a9
Michael-Nano <z940109765@qq.com>
2020-09-23 22:29:18 +0800

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,30 @@
+import logging
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__, template_folder='../templates')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors
